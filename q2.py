1d218f93a03a495a184cb8198fd4b8f7c1a732f2
EC2 Default User <ec2-user@ip-172-31-89-10.ec2.internal>
2021-12-05 17:06:31 +0000

    the cluster died wtf

--- q2.py
+++ q2.py
@@ -1,5 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, desc, length, asc, lit
 
 # you may add more import if you need to
 
@@ -9,3 +10,19 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 2").getOrCreate()
 # YOUR CODE GOES BELOW
+
+df = spark.read.option("header",True)\
+    .option("inferSchema", True)\
+    .csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
+
+# Drop the rows that have Price Range as null
+df = df.na.drop(subset=["Price Range"])
+df.printSchema()
+feature_group = ["Price Range", "City"]
+maxSet = df.groupBy(feature_group).agg({"Rating": "max"}).withColumn("dummy", lit("dummmy"))
+df_join = maxSet.join(df, on=['Price Range', "City"], how='left')
+df_join.drop_duplicates(["City","Price Range"]).show()
+# joinedMax = df.join(maxSet,feature_group).dropDuplicates().show()
+
+# df_agg = df.groupby('date').agg(F.max('close').alias('close_agg')).withColumn("dummy",F.lit("dummmy")) # dummy column is needed as a workaround in spark issues of self join
+# df_join = df.join(df_agg,on='date',how='left')
